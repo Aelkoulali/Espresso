e178dfa54def5b1a5c0ce613097879e4dc05aa3a
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-22 20:02:12 +0200

    fix formatting and remove extra whitespace in products.js

--- scripts/products.js
+++ scripts/products.js
@@ -3,6 +3,7 @@ const products = [
   { id: 2, name:"Bagel Cheese Cream" , price: 2.50, category:food,type:bakery,popular:false, image:"images/Menu/bakery/Bagel_Cheese_Cream.png"},
   { id: 3, name:"Cinnamon Roll" , price: 2.50, category:food,type:bakery,popular:true, image:" images/Menu/bakery/Cinnamon_Roll.png"},
   { id: 4, name:"Pain au Chocolat" , price: 2.50, category:food,type:bakery,popular:false, image:" images/Menu/bakery/Pain_au_Chocolat.png"},
+ 
   
   
 ]
