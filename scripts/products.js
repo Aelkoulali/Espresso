d800de97d7f6d2be0a25441be6ff37320a859967
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-23 11:56:01 +0200

    fix formatting by adding missing quotes around category and type in products.js

--- scripts/products.js
+++ scripts/products.js
@@ -1,8 +1,8 @@
 const products = [
-  { id: 1, name:"Almond Croissant" , price: 2.50, category:food,type:bakery,popular:true, image:" images/Menu/bakery/Almond_Croissant.png"},
-  { id: 2, name:"Bagel Cheese Cream" , price: 2.75, category:food,type:bakery,popular:false, image:"images/Menu/bakery/Bagel_Cheese_Cream.png"},
-  { id: 3, name:"Cinnamon Roll" , price: 2.00, category:food,type:bakery,popular:true, image:" images/Menu/bakery/Cinnamon_Roll.png"},
-  { id: 4, name:"Pain au Chocolat" , price: 3.00, category:food,type:bakery,popular:false, image:" images/Menu/bakery/Pain_au_Chocolat.png"},
+  { id: 1, name:"Almond Croissant" , price: 2.50, category:"food",type:"bakery",popular:true, image:" images/Menu/bakery/Almond_Croissant.png"},
+  { id: 2, name:"Bagel Cheese Cream" , price: 2.75, category:"food",type:"bakery",popular:false, image:"images/Menu/bakery/Bagel_Cheese_Cream.png"},
+  { id: 3, name:"Cinnamon Roll" , price: 2.00, category:"food",type:"bakery",popular:true, image:" images/Menu/bakery/Cinnamon_Roll.png"},
+  { id: 4, name:"Pain au Chocolat" , price: 3.00, category:"food",type:"bakery",popular:false, image:" images/Menu/bakery/Pain_au_Chocolat.png"},
  
   
   
