7af3f8ac5d00a769f3e396c915724e3fbca321b6
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-23 19:01:13 +0200

    add additional bakery products to products.js

--- scripts/products.js
+++ scripts/products.js
@@ -3,6 +3,12 @@ const products = [
   { id: 2, name:"Bagel Cheese Cream" , price: 2.75, category:"food",type:"bakery",popular:false, image:"images/Menu/bakery/Bagel_Cheese_Cream.png"},
   { id: 3, name:"Cinnamon Roll" , price: 2.00, category:"food",type:"bakery",popular:true, image:" images/Menu/bakery/Cinnamon_Roll.png"},
   { id: 4, name:"Pain au Chocolat" , price: 3.00, category:"food",type:"bakery",popular:false, image:" images/Menu/bakery/Pain_au_Chocolat.png"},
+  { id: 5, name:"Almond Croissant" , price: 2.50, category:"food",type:"bakery",popular:true, image:" images/Menu/bakery/Almond_Croissant.png"},
+  { id: 6, name:"Bagel Cheese Cream" , price: 2.75, category:"food",type:"bakery",popular:false, image:"images/Menu/bakery/Bagel_Cheese_Cream.png"},
+  { id: 7, name:"Cinnamon Roll" , price: 2.00, category:"food",type:"bakery",popular:true, image:" images/Menu/bakery/Cinnamon_Roll.png"},
+  { id: 8, name:"Pain au Chocolat" , price: 3.00, category:"food",type:"bakery",popular:false, image:" images/Menu/bakery/Pain_au_Chocolat.png"},
+  
+ 
  
   
   
