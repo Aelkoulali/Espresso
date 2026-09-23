05965865f0467e3bfdb46ad2ce67c20fc3bb21aa
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-23 19:07:32 +0200

    update prices and categories for lunch items in products.js

--- scripts/products.js
+++ scripts/products.js
@@ -3,10 +3,10 @@ const products = [
   { id: 2, name:"Bagel Cheese Cream" , price: 2.75, category:"food",type:"bakery",popular:false, image:"images/Menu/bakery/Bagel_Cheese_Cream.png"},
   { id: 3, name:"Cinnamon Roll" , price: 2.00, category:"food",type:"bakery",popular:true, image:" images/Menu/bakery/Cinnamon_Roll.png"},
   { id: 4, name:"Pain au Chocolat" , price: 3.00, category:"food",type:"bakery",popular:false, image:" images/Menu/bakery/Pain_au_Chocolat.png"},
-  { id: 5, name:"Chicken Mozarella Panini" , price: 2.50, category:"food",type:"bakery",popular:true, image:" images/Menu/Lunch/Chicken_Mozarella_Panini.png"},
-  { id: 6, name:"Chicken Parmigiana" , price: 2.75, category:"food",type:"bakery",popular:false, image:"images/Menu/Lunch/Chicken_Parmigiana.png"},
-  { id: 7, name:"Turkey Cranberry Brie" , price: 2.00, category:"food",type:"bakery",popular:true, image:" images/Menu/Lunch/Turkey_Cranberry_Brie.png"},
-  { id: 8, name:"Wrap Chicken Avocado" , price: 3.00, category:"food",type:"bakery",popular:false, image:" images/Menu/Lunch/Wrap_Chicken_Avocado.png"},
+  { id: 5, name:"Chicken Mozarella Panini" , price: 7.50, category:"food",type:"lunch",popular:true, image:" images/Menu/Lunch/Chicken_Mozarella_Panini.png"},
+  { id: 6, name:"Chicken Parmigiana" , price: 8.00, category:"food",type:"lunch",popular:false, image:"images/Menu/Lunch/Chicken_Parmigiana.png"},
+  { id: 7, name:"Turkey Cranberry Brie" , price: 9.00, category:"food",type:"lunch",popular:true, image:" images/Menu/Lunch/Turkey_Cranberry_Brie.png"},
+  { id: 8, name:"Wrap Chicken Avocado" , price: 8.00, category:"food",type:"lunch",popular:true, image:" images/Menu/Lunch/Wrap_Chicken_Avocado.png"},
   
  
  
